1caf2a9deef73850e2e1cfcea2131176d4681771
maddyonline <maddyonline@users.noreply.github.com>
2023-04-28 21:06:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-skqdw8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-skqdw8)
---
+++ pages/index.js
@@ -0,0 +1,79 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import Stories from 'react-insta-stories';
+
+export default function Home() {
+  const stories = [
+    'https://images.unsplash.com/photo-1632370275859-0be3361ca6e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80',
+    {
+      url: 'https://images.unsplash.com/photo-1632393712881-61f6e11ac65e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80',
+      duration: 5000,
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Cool <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <Stories
+          stories={stories}
+          defaultInterval={1500}
+          width={432}
+          height={768}
+        />
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+        </a>
+      </footer>
+    </div>
+  );
+}
